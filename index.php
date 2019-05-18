e7f8874efbd7e456fd472a18d070956f6158a361
Sheldon <s2maschmeyer@gmail.com>
2019-05-18 12:32:32 -0400

    Personal Website Backup

---
+++ index.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
+  <meta name="norton-safeweb-site-verification" content="7-7xiqq-b38mayk91gk7lju2b4m1yunwkwf3l2mip0fjm4i5un-1i-ky5lcuvpfymkjunspl1qnpc33wx2v1lxucn7f05s0hzxferv1lxifpvdfcccmskshpqh442zwy"> 
+
+  <title>Sheldon Maschmeyer</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/resume.min.css" rel="stylesheet">
+</head>
+<body id="page-top">
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <span class="d-block d-lg-none">Sheldon Maschmeyer</span>
+      <span class="d-none d-lg-block">
+        <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
+      </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container-fluid p-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+      <div class="w-100">
+        <h1 class="mb-0">Sheldon W. B. <span class="text-primary">Maschmeyer</span></h1>
+        <div class="subheading mb-5">284 Main Street · Ottawa, Ontario, Canada, K1S 1E1 · 1-613-889-4320 ·
+          <a href="mailto:sheldon@maschmeyer.ca">sheldon@maschmeyer.ca</a>
+        </div>
+        <p class="lead mb-5">• Creative &amp; Analytical Programmer<br>
+          <br>• Excellent researcher, with strong problem-solving skills.<br>
+          <br>• Strong organizational skills and ability to multi-task<br>
+          <br>• Exceptional relationship building and account management skills<br>
+          <br>• Sales and Customer Service Experience (B2C)<br>
+          <br>• Enhanced Reliability Security Clearance (Canada) Nov 2016 - Nov 2026<br>
+          <br>• I like to go bike riding, socialize with friends, Sci-fi, I look forward to travelling, enjoy reading and, much more.</p>
+        <div class="social-icons" style="width: 200px; float: left">
+          Professional Network:
+          <a href="https://www.linkedin.com/in/sheldonmaschmeyer/">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://github.com/sheldonmaschmeyer">
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+        <div class="social-icons" style="width: 200px; float: left">
+          Download Résumé:
+          <a href="data/resume.docx">
+            <i class="fas fa-file-word"></i>
+          </a>
+          <a href="data/resume.pdf">
+            <i class="fas fa-file-pdf"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="education">
+      <div class="w-100">
+        <h2 class="mb-5">Education</h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Bachelor of Science: Computer Science</h3>
+            <div class="subheading mb-3">Minor: Sociology · Carleton University</div>
+            <div>Major cGPA: 7.86 out of 12.00</div>
+            <p>Minor cGPA: 7.75 out of 12.00 <br> 
+              Overall cGPA: 7.48 out of 12.00</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">September 2015 - June 2019</span>
+          </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Bachelor of Science: Integrated Science</h3>
+            <div class="subheading mb-3">Concentration: Science and Technology · Carleton University</div>
+            <div>Major cGPA: </div>
+            <p>Concentration cGPA: <br> 
+            Overall cGPA: </p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">September 2009 - June 2014</span>
+          </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Secondary Schools</h3>
+            <div>Richmond Hill Adult Day School (Richmond Hill, Ontario) </div>
+            <p>Independent Learning Centre <br> 
+            Sir William Mulock Secondary School (Newmarket, Ontario) <br>
+            Thornhill Secondary School (Thornhill, Ontario) </p>
+          </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Elementary Schools</h3>
+            <div>E.J. Sand P.S. (Thornhill, Ontario) </div>
+            <p>Thornhill P.S. (Thornhill, Ontario) <br> 
+              Briergreen P.S. (Ottawa, Ontario) <br> 
+              Northview Elementary (Pointe Claire, Quebec) </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
+      <div class="w-100">
+        <h2 class="mb-5">Experience</h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">IT &amp; Media Arts (Part-Time)</h3>
+            <div class="subheading mb-3">I-AM Retailer Solutions / Abaan &amp; Associates</div>
+            <p>I-AM is a small Canadian startup specializing in advanced retail display solutions. I-AM targets large retail outlets and, the grocery industry.<br>
+              <br>Key Responsibilities: <br>
+              </p><ul> 
+                <li>Graphic art design/creation of multimedia business support materials, including concept renditions, situational prototype designs and presentation images, videos and animations </li>
+                <li>Created compelling sales demonstration content for executive level presentations and display on video screens and retail mobile devices. </li>
+                <li>Source, install, repair and maintain all corporate computers, accessories, etc.</li></ul><p></p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">June 2008 - Present</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Head Cashier (Part-Time)</h3>
+            <div class="subheading mb-3">Shoppers Drug Mart</div>
+            <p>Key Responsibilities:
+              </p><ul> 
+                <li>Efficiently and accurately operate cash register, OLG lottery, Load Presto Cards. </li>
+                <li>Promote retail promotions, contests and charity initiatives. </li>
+                <li>Performed merchandising responsibilities.</li></ul><p></p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">May 2016 - November 2018</span>
+          </div>
+        </div>
+    
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Field Training &amp; Support Representative<br>Field Service Representative</h3>
+            <div class="subheading mb-3">Dominion Voting Systems</div>
+            <p>Dominion Voting Systems began in 1895 with the invention of the first ever Direct Recording lever machines in New York. Dominion is still known for its innovation in election technology, leveraging its history of innovation through its vast pool of election specialists. Through partnering with and learning from top level suppliers and employees in the election automation industry,&nbsp;Dominion is taking part in the world’s most challenging and innovative democracy projects.</p>
+            <p>Key Responsibilities:</p>
+            <ul> 
+            <b>2018</b>
+            <li>Provided advanced training and on site (day of) support for the 2018 Ontario Election. </li>
+            <li>Trained all Poll Officials in Ottawa. </li>
+            <li>Assisted with equipment setup and conducted Logic &amp; Accuracy testing.</li>
+            <li>Provided support during polling hours and while votes were being tabulated.</li>
+            <b>2014</b>
+            <li>Assisted TDRO with regards to set-up.</li>
+            <li>Assisted TDRO with Logic and Accuracy testing of Dominion Voting Systems.</li>
+            <li>Ensured that assigned polling station was opened and closed on time.</li>
+            <li>Ensured that all voting machines remained operational throughout all polling days.</li></ul><p></p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">July 2010 - December 2011</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Customer Service Representative</h3>
+            <div class="subheading mb-3">Canada Computers</div>
+            <p>Ontario and Quebec computers and electronics retail store.</p>
+            <p>Key Responsibilities:
+              </p><ul> 
+                <li>Service Desk representative responsible for aiding clients with troubleshooting technical issues and suggesting appropriate solutions.</li>
+                <li>Responsible for assisting clients in finding the right software or hardware to meet their needs.</li>
+                <li>Used POS system to order required services and parts for pickup or delivery as well as inventory transfer between customer service department and sales (for custom builds and repairs).</li></ul><p></p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">June 2014 - August 2015</span>
+          </div>
+        </div>
+        
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Deputy Returning Officer (By-Election)</h3>
+            <div class="subheading mb-3">Elections Canada</div>
+            <p>Key Responsibilities:
+              </p><ul> 
+                <li>Responsible for setting up the polling station with necessary supplies.</li>
+                <li>Ensure voters provided proof of identity and address before voting as well as provide ballots to voters.</li>
+                <li>Ensured all polling clerks and party representatives followed all rules and procedures.</li>
+                <li>Managed registration process at polling station when required.</li>
+                <li>Tabulated votes, completed all required paper work, informed Elections Canada of polling station voting results and returned the ballot box and related materials to the Returning Officer.</li></ul><p></p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">September 2014</span>
+          </div>
+        </div>
+        
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Sales Associate</h3>
+            <div class="subheading mb-3">Fox Wireless Comm. (Now Telus)</div>
+            <p>Key Responsibilities:
+              </p><ul> 
+                <li>Responsible for meeting and exceeding sales quota of Telus Mobile devices, contracts and accessories.</li>
+                <li>Responsible for assisting in store opening and closing duties.</li>
+                <li>Responsible for ensuring a clean and welcoming environment for customers.</li></ul><p></p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">May 2008 - August 2008</span>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+    
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
+      <div class="w-100">
+        <h2 class="mb-5">Skills</h2>
+
+        <div class="subheading mb-3">Programming Languages &amp; Tools</div>
+        <ul class="list-inline dev-icons">
+          <li class="list-inline-item">
+            <i class="fab fa-java" data-toggle="tooltip" title="Java"></i>
+          </li>
+          <li class="list-inline-item">
+            <img src="img/c.png" data-toggle="tooltip" title="C Programming Language">
+          </li>
+          <li class="list-inline-item">
+            <img src="img/cpp.png" data-toggle="tooltip" title="C++ Programming Language">
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-html5" data-toggle="tooltip" title="HTML5"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-css3-alt" data-toggle="tooltip" title="CSS3"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-js-square" data-toggle="tooltip" title="JavaScript"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-python" data-toggle="tooltip" title="Python"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-node" data-toggle="tooltip" title="NodeJS"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-npm" data-toggle="tooltip" title="Node Package Manager"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-linux" data-toggle="tooltip" title="Linux OS"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-windows" data-toggle="tooltip" title="Windows OS"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fas fa-network-wired" data-toggle="tooltip" title="Network security: Burp, Wireshark tools"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fas fa-terminal" data-toggle="tooltip" title="Shell Script, Bash, Batch, Terminal, CMD, PowerShell"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fas fa-database" data-toggle="tooltip" title="Access, SPSS, Oracle SQL, mySQL, MongoDB"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-microsoft" data-toggle="tooltip" title="Office: Word, Excel, Outlook, PowerPoint, Publisher"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-adobe" data-toggle="tooltip" title="Adobe: Photoshop, Premier, Acrobat, Dreamweaver"></i>
+          </li>
+          <li class="list-inline-item">
+            <img src="img/latex.png" data-toggle="tooltip" title="LaTeX: Used for all computer science documents">
+          </li>
+        </ul>
+      </div>
+    </section>
+    
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="services">
+      <div class="w-100">
+        <h2 class="mb-5">Maschmeyer Services
+        <div class="subheading mb-3">Computer Repair &amp; Traiing <br> IT &amp; Media Arts Services</div></h2>
+        <p>In Thornhill and Newmarket, I provided computer repair and training services to members of the community.<br>
+        I continue to provide the same services, in my spare time, in Ottawa.</p>
+        <p>Call or Email for a Free Consultation and Estimates <br> Personalized Service: On-Site or Drop-off <br>
+        <b>100% Customer Satisfaction Guaranteed</b></p>
+        <p>10+ Years of Experience Providing Computer Related Services<br> 
+          References &amp; Testimonials Available Upon Request.<br>
+        Masters Busienss License (MBL) for Ontario: 270798713</p>
+      </div>
+    </section>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/resume.min.js"></script>
+  
+  <script>
+		$(document).ready(function(){
+  		$('[data-toggle="tooltip"]').tooltip();
+		});
+  </script>
+
+
+
+</body></html>

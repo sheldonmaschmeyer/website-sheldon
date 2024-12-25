1794b956973f1395c396a789d9a906c34163f720
Sheldon Maschmeyer <sheldon@maschmeyer.ca>
2024-12-24 22:30:46 -0500

    Update volunteer items

--- src/sectionItems/volunteerItems.vue
+++ src/sectionItems/volunteerItems.vue
@@ -1,8 +1,60 @@
 <template>
   <div :class="classes.item">
     <div class="resume-content white-space: pre-wrap;">
+      <div class="subheading">2024</div>
+      <div class="ps-4">
+        ▪ Donated to UNRWA <br />
+        The terrorist attack on Israel is horrific. <br />
+        The organization was compromised by a few extremists who carried out, or
+        facilitated, the horrible murders, rapes and kidnappings. The United
+        Nations Relief and Works Agency is one of the only organizations that is
+        still operating in war zones within Gaza. <br />
+        They have fired the bad apples who were involved with the terrorist
+        attacks on Israel and the Canadian government, after an investigation,
+        actively donates to UNRWA. <br />
+        War is a form of hell on earth. Innocent civilians, many children, need
+        food, medicine and shelter. Since the terrorists hide and fight amongst
+        civilians, using them as human shields, the task of humanitarian
+        assistance is precarious.
+      </div>
+      <div class="ps-4">
+        ▪ Donated to the Ottawa Food Bank <br />
+        I live downtown Ottawa. There are pockets poverty and hunger throughout
+        Canada. What separates us from a homeless, malnourished individual is a
+        thin-red-line. <br />
+        I donate money rather than groceries to food banks since, the charities
+        are able to use the funds for the services and items that are urgently
+        needed. Before university, I volunteered at the Aurora Food Pantry. I
+        went through boxes, throwing out expired items. They would have an
+        abundance of some items such as peanut butter but, limited in items for
+        children and adult dietary requirements such as fresh milk and eggs. The
+        food bank, through monetary donation, may purchase what is urgently
+        required rather than what we think they need.
+      </div>
+      <div class="ps-4">
+        ▪ Donated to the Provincial Liberal Party <br />
+        Doug Ford has been in office for almost seven years and his party's
+        cronyism is only getting worse. It is time for a change of government.
+        <br />
+        I believe that Bonnie Crombie will be a fantastic Premier of Ontario.
+        Challenging a potential future federal Conservative government. <br />
+        Like my father, I often work on for start-ups, on contract, with no
+        insurance or, costly monthly private insurance. I am fortunate to be in
+        good health. <br />
+        Fighting for universal healthcare, reducing wait times and increasing
+        accessibility to family doctors. I do not currently have a family doctor
+        but, I am again on a government wait list. My previous doctor moved and,
+        was at a costly distance (via. Uber) and time-consuming distance from my
+        place of residence. <br />
+        Improved affordability of housing and rent for both the middle class
+        and, low income families. <br />
+        While I greatly respect the NDP and the Green Party, in Ontario, I
+        believe that the Liberals, in the long-term, have the best chance to
+        form a stable government that can implement important legislation and
+        lasting change.
+      </div>
       <div class="subheading">IMRSV</div>
-      • Donated to Canadian Red Cross for:
+      • Donated to Canadian Red Cross for:11
       <div class="ps-4">▪ COVID-19 aid in India</div>
       <div class="ps-4">▪ Canadian flood or fires</div>
       <div class="ps-4">▪ Urgent aid to Ukraine</div>

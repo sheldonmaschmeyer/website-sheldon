7b8fc27c787c40beacd3a9adce9ed5ff1d04204b
Sheldon Maschmeyer <sheldon@maschmeyer.ca>
2024-12-24 22:30:46 -0500

    Update volunteer items and donations

--- src/sectionItems/lifeGoalsItems.vue
+++ src/sectionItems/lifeGoalsItems.vue
@@ -1,13 +1,8 @@
 <template>
   <div>
     <CollapseSubheading heading-text="High Priority" target="#highPriority" />
-    <div id="highPriority" class="collapse ps-4">
-      • Reduce debit. <br />
-      • When I started at ANVIL my credit card debit was above $30K. When I left
-      ANVIL, my debit was under $10K. <br />
-      • For risk and adventure, I have invested in Alpha-El Inc. I have taken
-      temporary random pay-cuts and skipped payments. I am paid between $0 and
-      $6,000 GROSS monthly. Therefore, my debit increased to ~$45K. <br />
+    <div id="highPriority" class="collapse ps-4 show">
+      • Reduce debt, while slowly paying off student loans. <br />
       • A new career employment opportunity must be a steady dependable
       pay-cheque. Preferably not as a contractor but with benefits at $75K+ per
       annam.
@@ -15,12 +10,11 @@
   </div>
   <div>
     <CollapseSubheading heading-text="This year" target="#shortTerm" />
-    <div id="shortTerm" class="collapse ps-4">
-      • An economical electric vehicle <br />
+    <div id="shortTerm" class="collapse ps-4 show">
+      • To get married and start a family.<br />
       <span class="lf"
-        >-> My partner and I would find a vehicle convenient, especially when
-        starting a family. The current apartment build we are in has electric
-        charging stations
+        >->Plan a wedding in Canada and Philippines, my partner's childhood
+        home.
       </span>
     </div>
   </div>
@@ -29,13 +23,13 @@
       heading-text="One to three years"
       target="#mediumTerm"
     />
-    <div id="mediumTerm" class="collapse ps-4">
+    <div id="mediumTerm" class="collapse ps-4 show">
       • Pay-off Credit Card: <br />
       <span class="lf"
         >-> Used for educational expenses, a small business and, other personal
         expenses.</span
       ><br />
-      • Start a family. <br />
+      • Own a house in Ottawa or Toronto area. <br />
       • A first mortgage which, is an investment. <br />
       <span class="lf"
         >->If getting a variable rate mortgage during high inflation: </span
@@ -55,9 +49,8 @@
   </div>
   <div>
     <CollapseSubheading heading-text="Within seven years" target="#longTerm" />
-    <div id="longTerm" class="collapse ps-4">
-      • Pay off student loan. Currently renewing repayment assistance.<br />
-      • Invest for family and old age.<br />
+    <div id="longTerm" class="collapse ps-4 show">
+      • Pay off student loan. Currently renewing repayment assistance. <br />
     </div>
   </div>
 </template>

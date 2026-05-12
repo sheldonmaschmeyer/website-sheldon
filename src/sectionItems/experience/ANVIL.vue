c42e500e85e52f25807f67416c624b19017650cb
Sheldon Maschmeyer <sheldon.maschmeyer@alpha-el.ca>
2026-05-12 15:36:51 -0400

    Update experience section

---
+++ src/sectionItems/experience/ANVIL.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    Full Stack Developer for web and mobile (Android and iPhone) app development,
+    working on projects that required a Level III Top Secret clearance for the
+    Canadian government.
+    <div class="pt-3">
+      • Website and mobile, Android and iPhone, app development.<br />
+      • Gained experience working with agile teams.<br />
+      • Accumulated experience with REST APIs and front-end development utilizing
+      JavaScript frameworks such as React Native, React, and Vue.<br />
+      • Back-end PostgreSQL databases where I wrote and tested migrations and
+      troubleshot issues.<br />
+      • Set up Docker environments using Docker Compose.<br />
+      • Wrote unit and end-to-end tests to ensure product quality and test for
+      edge cases.<br />
+      • Utilized TypeScript for static typing, classes, and interfaces.<br />
+      • Set up ESLint and StyleLint for code quality, consistency, and
+      readability.
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ANVILSummary',
+};
+</script>
--- src/sectionItems/experience/AlphaElChief.vue
+++ src/sectionItems/experience/AlphaElChief.vue
@@ -2,17 +2,31 @@
   <div>
     ALPHA-EL Inc. is a "First Nations owned and operated AI and machine learning
     Consulting Company, that is interested in working in different geographic
-    regions in Canada." <br />
-    <br />
-
-    Working remotely in Ottawa. Available to go on-site in Whitehorse when
-    requested. <br />
-    <br />
-
-    I am a CTO working on project RAVEN EYE: A Geo-intelligence Optical AI
-    Surveillance Capability System. <br />
-    Working remotely in Ottawa. Available to go on-site in Whitehorse when
-    requested.
+    regions in Canada." Working remotely in Ottawa. Available to go on-site in
+    Whitehorse when requested.
+    <br /><br />
+    Began as a Consultant before being promoted to Chief Technology Officer on
+    project CORAX VISION: A Geo-intelligence Optical AI Surveillance Capability
+    System.
+    <div class="pt-3">
+      • Website and mobile Web App (Full Stack) development: React, Vite,
+      Zustand, Express, PostgreSQL.<br />
+      • Training and implementing a neural network (YOLO frameworks) to detect
+      wildlife, fire, and smoke.<br />
+      • Built custom modules for runtime features that integrate with the
+      camera.<br />
+      • Supervised interns assisting with an automated script for trail camera
+      footage and experiments such as fire simulations.<br />
+      • Using Docker containers for each service and integrating all software
+      system components.<br />
+      • Listening to client needs and implementing new features in Python and the
+      full stack web application, including data science tools.<br />
+      • Integrating appropriate AWS services (EC2, S3, Kinesis Video) for
+      data/video/assets storage and reliability.<br />
+      • Platform-agnostic philosophy utilizing hardware acceleration: NVIDIA
+      Jetson edge devices (ARM64). Docker containers ensure code runs on both
+      x64 and ARM architectures.
+    </div>
   </div>
 </template>
 
